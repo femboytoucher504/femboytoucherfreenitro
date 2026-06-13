f22137111f24e71ea4bc7223c8e23fb53e8d043a
femboytoucher504 <ahmemuhsins3169@gmail.com>
2026-06-13 16:55:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-({
+export default {
     onLoad: () => {
-        const api = typeof revenge !== 'undefined' ? revenge : (typeof vendetta !== 'undefined' ? vendetta : null);
+        const api = window.revenge || window.vendetta;
         if (!api) return;
-
         try {
             const EmojiUtils = api.metro.findByProps("canUseEmojis") || api.metro.findByProps("getEmojiUnavailableReason");
             if (EmojiUtils) {
                 if (EmojiUtils.canUseEmojis) EmojiUtils.canUseEmojis = () => true;
                 if (EmojiUtils.canUseAnimatedEmojis) EmojiUtils.canUseAnimatedEmojis = () => true;
             }
-        } catch (e) {}
+        } catch (e) {
+            console.error(e);
+        }
     },
     onUnload: () => {}
-})
+};
